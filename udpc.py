c399d9158642d71398b8726930fc4833eefb4935
aszer <aszer27937@gmail.com>
2016-12-24 19:53:49 +0800

    Update udpc.py

--- udpc.py
+++ udpc.py
@@ -1,7 +1,8 @@
 import socket
+ipaddr=""#your ip address
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
-    s.sendto("client",('49.140.77.39',9999))
+    s.sendto("client",(ipaddr,9999))
     data,addr = s.recvfrom(1024)
     if data:
         break
